916a64e029aa5ce1a8cb9767317faddbeccf6316
Ysanj324 <sanjit.yagnik@gmail.com>
2024-12-31 19:46:42 +0530

    test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+dotenv.config();
 
-const app =express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+// Routes
+app.use("/api/user",userRoutes);
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000!!");
 });
